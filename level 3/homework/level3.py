86d464e88d1509ebde2e582761441e70574619e4
nikakobaxa <kobaxanika89@gmail.com>
2024-05-25 04:14:30 -0700

    goa

---
+++ level 3/homework/level3.py
@@ -0,0 +1,24 @@
+name = "nika"
+username = "kobakhidze"
+age = 15
+height = 1.79
+
+
+print(name + " " + username + " " + str(age) + " " + str(height))
+
+
+num1 = 20
+num2 = 30
+num3 = 12.5
+
+#print(20 + 30)
+print(30 * 12.5)
+
+name = "luka"
+username = "kobakhidze"
+is_fotballer = True
+
+print(type(name))
+print(type(username))
+print(type(is_fotballer))
+
