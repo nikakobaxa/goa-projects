39c46ad119a650e944807efcbb6a625e58ff1a95
nikakobaxa <kobaxanika89@gmail.com>
2024-05-26 08:00:34 -0700

    goa

---
+++ level 2/homework/lesson2.py
@@ -0,0 +1,19 @@
+name = "nika"
+username = "yubaneishvili"
+
+print(name + " " + username)
+
+name1 = "andria"
+username1 = "farcvania"
+
+print(name1 + " " + username1)
+
+name2 = "giorgi"
+username2 = "kupatadze"
+
+print(name2 + " " + username2)
+
+name3 = "andria"
+username3 = "janjulia"
+
+print(name3 + " " + username3)
