c29e87ae00b0d5991ca4dc26ab1e96e93f95262d
nikakobaxa <kobaxanika89@gmail.com>
2024-06-01 04:30:06 -0700

    goa

---
+++ level 4/classwork/lesson4.py
@@ -0,0 +1,20 @@
+# name = "data"
+# surname = "diasamidze"
+# age = 100
+
+# print(name + " " +  surname  + " " + str(age))
+
+number = 5
+number = number + 1
+
+print(number)
+
+balance = 150
+discount = 0.6 
+
+total = balance - balance * discount
+
+print(total)
+
+
+
