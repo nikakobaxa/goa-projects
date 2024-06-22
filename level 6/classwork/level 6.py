856bd548f18fe177e4c8a3cedb9cfb8badbb563b
nikakobaxa <kobaxanika89@gmail.com>
2024-06-22 00:55:07 -0700

    second commit

---
+++ level 6/classwork/level 6.py
@@ -0,0 +1,21 @@
+# name = input("enter your name: ")
+
+# print = ("your name is" + " " + name)
+
+num1 = int(input("pls enter the first number: "))
+num2 = int(input("pls enter the second number: "))
+num3 = int(input("pls enter the three number: "))
+
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 // num2 // num3)
+
+
+# name = input("enter your name: ")
+# lastname = input("enter your lastname: ")
+# age = input("enter your age: ")
+
+# print(f"your name is {name} your lastname is {lastname} your age is {age}")
+
+
---
+++ level 6/homework/level6h.py
@@ -0,0 +1,27 @@
+
+# name = input("enter your name: ")
+# lastname = input("enter your lastname: ")
+# age = input("enter your age: ")
+# gmail = input("enter your gmail: ")
+
+# print(f"your name is {name} your lastname is {lastname} your age is {age} your gmail is {gmail}")
+
+
+# num1 = int(input("pls enter the first number: "))
+# num2 = int(input("pls enter the second number: "))
+# num3 = int(input("pls enter the three number: "))
+
+# print(num1 + num2 + num3)
+# print(num1 - num2 - num3)
+# print(num1 * num2 * num3)
+# print(num1 // num2 // num3)
+
+
+# item_price = int(input("enter the price: "))
+# amount = int(input("Enter the quantity: "))
+
+# print(amount * item_price)
+
+car_type = "sedan"
+print(sedan)
+
