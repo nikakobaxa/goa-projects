a6d0d66262a5fd4f57fa9228a8ba17b75fabaa99
nikakobaxa <kobaxanika89@gmail.com>
2024-06-29 04:15:04 -0700

    second commit

---
+++ level 8/classwork/test.py
@@ -0,0 +1,63 @@
+# greating = "hello world"
+# number = 530
+# number1 = 13.5
+# is_true = False
+
+
+# print(type(greating))
+# print(type(number))
+# print(type(number1))
+# print(type(is_true))
+
+# num = int(input("Enter number: "))
+
+# print(num)
+# print(type(num))
+
+
+# print(5 < 1)
+# print(6 < 1)
+# print(7 < 9)
+
+# print(1 > 0) 
+# print(2 > 1)
+# print(4 > 5)
+
+# print(5 == 5) 
+# print(6 == 6)
+# print(7 == 7)
+
+# print(5 != 6) 
+# print(7 != 8)
+# print(2 != 1)
+
+# print(5 >= 5)
+# print(6 >= 5)
+# print(7 >= 4)
+
+# print(5 <= 5) 
+# print(6 <= 6)
+# print(7 <= 9)
+
+# soil_moisture = 80
+
+# print(soil_moisture < 100)
+
+
+# num1 = 20
+
+# print(num1 < 2)
+
+# num1 = 1
+
+# print(num1 < 2)
+
+num1 = int(input("Enter your number: "))
+num2 = int(input("Enter your second number: "))
+
+print(num1 < num2 )
+print(num1 > num2 )
+print(num1 == num2 )
+print(num1 != num2 )
+print(num1 >= num2 )
+print(num1 <= num2 )
---
+++ level 8/homework/homework.py
@@ -0,0 +1,43 @@
+# num1 = int(input("Enter your number: "))
+# num2 = int(input("Enter your second number: "))
+
+# print(num1 < num2 )
+# print(num1 > num2 )
+# print(num1 == num2 )
+# print(num1 != num2 )
+# print(num1 >= num2 )
+# print(num1 <= num2 )
+
+# print(3 < 2) #false
+# print(7 < 4) #false
+# print(3 < 5) #true
+
+# print(2 > 3) #false
+# print(5 > 4) #true
+# print(6 > 5) #true
+
+# print(4 == 4) #true
+# print(2 == 2) #true
+# print(9 == 9) #true
+
+# print(4 != 5) #true
+# print(4 != 9) #true
+# print(2 != 1) #true
+
+# print(4 >= 4) #true
+# print(7 >= 3) #true
+# print(8 >= 6) #true
+
+# print(2 <= 2) #true
+# print(3 <= 2) #false
+# print(7 <= 8) #true
+
+# num1 = int(input("Enter your number: "))
+# num2 = int(input("Enter your second number: "))
+
+# print(num1 < num2 )
+# print(num1 > num2 )
+# print(num1 == num2 )
+# print(num1 != num2 )
+# print(num1 >= num2 )
+# print(num1 <= num2 )
