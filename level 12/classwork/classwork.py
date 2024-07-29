6b7b3e428c00c430ac944f8a39a5bec949399093
nikakobaxa <kobaxanika89@gmail.com>
2024-07-29 07:55:16 -0700

    second commit

---
+++ level 12/classwork/classwork.py
@@ -0,0 +1,26 @@
+# age = 17
+
+# if age >= 18:
+#     print("regular price")
+# else:
+#     print("discount")
+
+
+# number = int(input("Enter a number: "))
+
+# if number == 6:
+#    print("you won house")
+# elif number == 30:
+#     print("you won ticket havai")
+# else:
+#     print("you won 1 dollar")
+
+# age = 17
+# students_age = 16
+# if age >= 18:
+#     print("regular price")
+# elif students_age >= 18:
+#     print("regular prices")
+# else:
+#     print("discounts")
+
---
+++ level 12/homework/homework.py
@@ -0,0 +1,48 @@
+# i = 1
+# while i <= 100:
+#     if 50 <= i <= 60:
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# sum = 0
+# number = 1
+
+# while sum < 50:
+#     sum += number
+#     number += 1
+
+# print(sum)
+# print(number - 1)
+
+
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print("რიცხვი ლუწია.")
+# else:
+#     print("რიცხვი კენტია.")
+
+
+# score = int(input("Enter a number: "))
+
+# if 90 <= score <= 100:
+#     print("Grade A")
+# elif 80 <= score <= 89:
+#     print("Grade B")
+# elif 70 <= score <= 79:
+#     print("Grade C")
+# elif 60 <= score <= 69:
+#     print("Grade D")
+# elif 0 <= score <= 59:
+#     print("Grade F")
+
+# age = int(input("Enter your age: "))
+
+# if age < 13:
+#     print("you are kid")
+# elif age > 13 and age < 20:
+#     print("you are teenager")
+# elif age > 20:
+#     print("you are grown up")
