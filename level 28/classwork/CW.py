66d18d4699d8362db5f46a7a60df321498f9bc9c
nikakobaxa <kobaxanika89@gmail.com>
2024-11-20 10:13:58 -0800

    second commit

---
+++ level 28/classwork/CW.py
@@ -0,0 +1,13 @@
+numbers = [i for i in range(1, 101)]
+print(numbers)
+
+numbers1 = [i for i in range(1, 101) if i % 2 == 0]
+print(numbers1)
+
+names = ["daviti", "nika", "luka"]
+names = [i.upper() for i in names]
+print(names)
+
+numbers2 = [i ** 2 for i in range(1, 101)]
+print(numbers2)
+
