6ef6ca99dc2ff7f89248215b1a2003dc83e44f02
nikakobaxa <kobaxanika89@gmail.com>
2025-02-24 01:39:11 -0800

    second commit

---
+++ level 41/Classwork/script.js
@@ -0,0 +1,17 @@
+function greet(){
+    console.log("Hello World!")
+}
+
+greet()
+
+function sum(num1,  num2,  num3){
+    console.log(num1 + num2 + num3)
+}
+
+sum(10, 50, 100)
+
+function defaultparametr(name = "nika", age = "16"){
+    console.log(name + age)
+}
+
+defaultparametr()
---
+++ level 41/Homework/script.js
@@ -0,0 +1,11 @@
+let userage = prompt("Enter your age:")
+let username = prompt("Enter Your Name:")
+
+console.log(userage)
+console.log(username)
+
+function explaindom(){
+    console.log("Dom is html tree which is sorted hierarchical, in the browses dom allows you to make interacations in html with java script")
+}
+
+explaindom()
