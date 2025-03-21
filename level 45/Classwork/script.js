aeffda14e35d6d515d172adbb99de631e2c99deb
nikakobaxa <kobaxanika89@gmail.com>
2025-03-21 08:00:22 -0700

    second commit

---
+++ level 45/Classwork/script.js
@@ -0,0 +1,39 @@
+// function createCar(brand, model, year, horsePower) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.year = year;
+//         this.horsePower = horsePower;
+
+//         this.increasehorsePower = function() {
+//             this.horsePower += 50;
+//         };
+// }
+
+// const car1 = new createCar("Toyota", "Corolla", 2020, 250);
+// const car2 = new  createCar("BMW", "X5", 2022, 300);
+// const car3 = new createCar("Mercedes", "C-Class", 2021, 450);
+
+// car1.increasehorsePower();
+// car2.increasehorsePower();
+// car3.increasehorsePower();
+
+// console.log(car1,car2,car3)
+
+// const myArray = [1, 2, 3, 4, 5];
+// for (let i = 0; i < myArray.length; i++){
+// console.log(myArray[i]);
+// }
+
+// const newArray = new Array(1, 2, 3, 4, 5);
+// for (let i = 0; i > newArray.length; i++){
+// console.log(newArray[i]);
+// }
+
+// const array = [1, 2, 3, 4, 5]
+
+console.log(array[0,3]);
+console.log(array.slice(1,3));
+
+
+// Associative arrays (ასოციაციური მასივები) — ეს არის მასივები, სადაც თითოეული ელემენტი არ ინახავს მხოლოდ ინდექსირებულ მნიშვნელობებს, არამედ აქვს გადმოცემული ნიშნული (key-value pairs). ეს ნიშნავს, რომ მონაცემებს არ უნდა ჰქონდეთ მხოლოდ რიცხვითი ინდექსები, არამედ შეიძლება ჰქონდეთ ნებისმიერი ტიპის კვანძი, როგორიცაა სტრიქონები, ობიექტები და ა.შ.
+// რატომ არ უნდა გამოვიყენოთ 1.შესრულების საკითხები 2.ცხადი ტიპები და კლასები 3.ბაგები და შეცდომები 4.ენა/ბიბლიოთეკა არ უჭერს მხარს 5.ალტერნატივები
---
+++ level 45/Homework/script.js
@@ -0,0 +1,29 @@
+// function Pc(brand, processor, ram, storage){
+//         this.brand = brand;
+//         this.processor = processor;
+//         this.ram = ram;
+//         this.storage = storage;
+// }
+
+// const pc1 = new Pc("Intel", "i3 10100F", 16, 1000);
+
+
+// function Keyboard(brand, type, isWireless){
+//         this.brand = brand;
+//         this.type = type;
+//         this.isWireless = isWireless;
+// }
+
+// const key = new Keyboard("Logitech", "Mechanical", true);
+
+
+// constructor (კონსტრუქტორი) არის სპეციალური ფუნქცია, რომელიც გამოიყენება ობიექტების შესაქმნელად და დასაწყისში მათთვის საწყისი
+
+
+const brands = ["Dell", "Intel i3",];
+const processor = ["Intel i3", "Intel i7"];
+const storage = [512, 64, 120];
+
+for (let i = 0; i < brands.length; i++);
+for (let i = 0; i < processor.length; i++);
+for (let i = 0; i < storage.length; i++);
